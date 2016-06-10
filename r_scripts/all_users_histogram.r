c083c5190e7a8e271c5e2aa1cd582c1f27ecf81e
ALIZA STONE <amstone326@gmail.com>
2016-06-10 15:00:30 -0400

    1st pass for user histograms done

--- r_scripts/all_users_histogram.r
+++ r_scripts/all_users_histogram.r
@@ -1,24 +1,27 @@
 
-MAX_DELTACS_IN_DAYS = 20
-MAX_DELTACS_IN_HOURS = 24*MAX_DELTACS_IN_DAYS
 MIN_NUM_FORMS = 100
 
 plotHistogram <- function() {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month_full.csv")
+	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
 	rowsOfInterest <- filterDataset(fullDataset)
-	hist(rowsOfInterest$mean_hours, breaks=getBucketsVector(), plot=TRUE, freq=TRUE)
+	hist(rowsOfInterest$median_hours, 
+		breaks=getBucketsVector(), 
+		plot=TRUE, freq=TRUE, col="lightblue",
+		main="Median DeltaCS by User",
+		xlab="Median DeltaCS (in hours)",
+		ylab="Count")
 }
 
 #' Return a filtered version of the original full dataset, excluding any rows that do not meet the following criteria:
-#'  - deltacs < MAX_DELTACS_IN_HOURS
-#-  - no. forms submitted > MIN_NUM_FORMS
+# - no. forms submitted > MIN_NUM_FORMS
 filterDataset <- function(dataset) {
-	imposeTimeLimit <- dataset[dataset$mean_hours <= MAX_DELTACS_IN_HOURS, ]
-	imposeFormsRequirement <- imposeTimeLimit[imposeTimeLimit$form_count >= MIN_NUM_FORMS, ]
+	imposeFormsRequirement <- dataset[dataset$form_count >= MIN_NUM_FORMS, ]
 	return(imposeFormsRequirement)
 }
 
 getBucketsVector <- function() {
-	return(c(seq(from=0, to=100, by=10), 150, 200, seq(from=300, to=500, by=100)))
+	#return(c(seq(from=0, to=100, by=10), 150, 200, seq(from=300, to=500, by=100)))
+	return(seq(from=0, to=1500, by=50))
+	#return(c(seq(from=0, to=500, by=50), seq(from=500, to=1500, by=200)))
 }
 
