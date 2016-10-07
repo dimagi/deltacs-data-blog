03a72717fd4ef7d8edf254240ab7f0db3de730fa
ALIZA STONE <amstone326@gmail.com>
2016-10-07 18:50:25 -0400

    have basic plot for all users, and started working on functions for plot by projects

--- r_scripts/all_users_graph_over_time.r
+++ r_scripts/all_users_graph_over_time.r
@@ -1,38 +1,66 @@
 
+fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
+
+
 plotScatterPlot <- function(useMean) {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
-	
-	xAxisData <- fullDataset$month
+	xAxisData <- seq(1,63)
 	if (useMean) {
-		yAxisData <- fullDataset$mean_hours
-		yLabel <- "Mean DeltaCS over a Month"
+		yAxisData <- as.numeric(t(getMeanDeltaCSByMonth()[2]))
+		yLabel <- "Mean DeltaCS over a Month (in hours)"
 	} else {
-		yAxisData <- fullDataset$median_hours
-		yLabel <- "Median DeltaCS over a Month"
+		yAxisData <- as.numeric(t(getMedianDeltaCSByMonth()[2]))
+		yLabel <- "Median DeltaCS over a Month (in hours)"
 	}
-	
-	#plot(xAxisData, yAxisData, xlab="Month", ylab=yLabel)
+	plot(xAxisData, yAxisData, pch = 20, col="blue", xlab="Month", ylab=yLabel)
+	#make custom axis
+	#make trendline -- abline(lm(xAxisData ~ yAxisData)) ?
 }
 
-getMeanAggregatedByMonth <- function() {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
-	aggregatedByMonth <- aggregate(fullDataset$mean_hours, list(Month = fullDataset$month), mean)
+# FUNCTIONS FOR PLOT BY PROJECT
+
+getLengthOfProjectInDays <- function(domainName) {
+	sortedMonthsForDomain <- sort(unique(fullDataset[fullDataset$domain == domainName,]$month))
+	lastMonth <- sortedMonthsForDomain[length(sortedMonthsForDomain)]
+	firstMonth <- sortedMonthsForDomain[1] 
+	return(as.numeric(as.Date(lastMonth) - as.Date(firstMonth)))
+}
+
+getLongestRunningProjects <- function(numProjects) {
+	projectsInOrderOfLength <- fullDataset[order(getLengthOfProjectInDays(fullDataset$domain))]
+	return(projectsInOrderOfLength)
+}
+
+
+# FUNCTIONS FOR ALL PLOT OF ALL USERS
+
+getMeanDeltaCSByMonth <- function() {
+	aggregatedByMonth <- aggregate(fullDataset$mean_hours, list(Month=fullDataset$month), mean)
 	return(aggregatedByMonth)
 }
 
-# this is definitely doing something weird, Jan and March 2016 don't look right
-getMedianAggregatedByMonth <- function() {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
-	aggregatedByMonth <- aggregate(fullDataset$median_hours, list(Month = fullDataset$month), median)
+getMedianDeltaCSByMonth <- function() {
+	aggregatedByMonth <- aggregate(fullDataset$median_hours, list(Month=fullDataset$month), median)
 	return(aggregatedByMonth)
+}
+
+# BASIC FUNCTIONS
+
+getNumUniqueUsers <- function() {
+	return(length(unique(fullDataset$user_id)))
+}
+
+getNumUniqueDomains <- function() {
+	return(length(unique(fullDataset$domain)))
+}
+
+getTotalForms <- function() {
+	return(sum(fullDataset$form_count))
 }
 
 getOverallMean <- function() {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
 	return(mean(fullDataset$mean_hours))
 }
 
 getOverallMedian <- function() {
-	fullDataset <- read.csv("./data/deltacs_stats_by_user_month.csv")
 	return(median(fullDataset$median_hours))
 }
