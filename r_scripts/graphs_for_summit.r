9c225bd58b9b697504d357b3ad65cedb8109bb9d
ALIZA STONE <amstone326@gmail.com>
2016-10-11 11:37:49 -0400

    compute all time diffs in months, to be more accurate

--- r_scripts/graphs_for_summit.r
+++ r_scripts/graphs_for_summit.r
@@ -6,15 +6,15 @@ datasetEndDate <- as.Date("2016-03-01")
 
 # FUNCTIONS FOR PLOT BY PROJECT
 
-getLengthOfProjectInDays <- function(domainName) {
+getLengthOfProjectInMonths <- function(domainName) {
 	sortedMonthsForDomain <- sort(unique(fullDataset[fullDataset$domain == domainName,]$month))
 	lastMonth <- sortedMonthsForDomain[length(sortedMonthsForDomain)]
 	firstMonth <- sortedMonthsForDomain[1] 
-	return(as.numeric(as.Date(lastMonth) - as.Date(firstMonth)))
+	return(getDurationInMonths(firstMonth, lastMonth))
 }
 
 getLongestRunningProjects <- function(numProjects) {
-	aggregatedByProjectLength <- aggregate(fullDataset$domain, list(Domain=fullDataset$domain), getLengthOfProjectInDays)
+	aggregatedByProjectLength <- aggregate(fullDataset$domain, list(Domain=fullDataset$domain), getLengthOfProjectInMonths)
 	sorted <- aggregatedByProjectLength[order(aggregatedByProjectLength$x),]
 	return(tail(sorted, n=numProjects))
 }
@@ -32,16 +32,16 @@ plotDeltaCSByMonthForProject <- function(domainName) {
 
 # FUNCTIONS FOR PLOT OF LONG-RUNNING USERS
 
-getDurationOfUserActivityInDays <- function(userID) {
+getDurationOfUserActivityInMonths <- function(userID) {
     sortedMonthsForUser <- sort(unique(fullDataset[fullDataset$user_id == userID,]$month))
     lastMonth <- sortedMonthsForUser[length(sortedMonthsForUser)]
     firstMonth <- sortedMonthsForUser[1]
-    return(as.numeric(as.Date(lastMonth) - as.Date(firstMonth)))
+    return(getDurationInMonths(firstMonth, lastMonth))
 }
 
 getUsersMeetingPercentageActivityThreshold <- function(percentageThreshold) {
-    totalDays <- as.numeric(datasetEndDate - datasetStartDate)
-    aggregatedByUserDuration <- aggregate(fullDataset$user_id, list(User=fullDataset$user_id), getDurationOfUserActivityInDays)
+    totalMonthsInDataset <- getDurationInMonths(datasetStartDate, datasetEndDate)
+    aggregatedByUserDuration <- aggregate(fullDataset$user_id, list(User=fullDataset$user_id), getDurationOfUserActivityInMonths)
     return(aggregatedByUserDuration[(aggregatedByUserDuration$x/totalDays) >= percentageThreshold,])
 }
 
@@ -63,7 +63,16 @@ plotDeltaCSOverMonthForAllUsers <- function(useMean) {
 }
 
 
-# BASIC FUNCTIONS
+# UTIL FUNCTIONS
+
+# Assumes that startMonth and endMonth are dates in the form of yyyy-mm-01, such that the date really
+# represents a month in which a project or user was active, as opposed to a specific date on which they started/ended
+getDurationInMonths <- function(startMonth, endMonth) {
+	diffInWeeks <- as.numeric(difftime(as.Date(endMonth), as.Date(startMonth), units=c("weeks")))
+	diffInMonths <- round(diffInWeeks/(52/12))  # divide by approx no. of weeks in a month
+	# Add 1 month because the endMonth should count as a full month, even though the date used to represent it is the first of the month
+	return(diffInMonths + 1) 
+}
 
 # in hours
 getMeanDeltaCSByMonth <- function(dataframe = fullDataset) {
